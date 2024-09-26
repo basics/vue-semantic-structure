0846c10404beb7eb865fb494d0e73007562305ef
Thorn Walli <lammpee@gmail.com>
2024-09-26 19:04:37 +0200

    fix(build): update build config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-export { default as ContentContainer } from './ContentContainer.vue';
-export { default as ContentHeadline } from './ContentHeadline.vue';
 export { default as useContentContainer } from './useContentContainer.js';
 export { default as useContentHeadline } from './useContentHeadline.js';
