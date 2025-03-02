eab8723cf05f10ab67922eb82aa822f6a808dc70
Thorn Walli <lammpee@gmail.com>
2025-03-02 17:53:23 +0100

    fix(exports): switch to named exports for better clarity

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export * from './ContentContainer';
-export * from './ContentHeadline';
+export { default as ContentContainer } from './ContentContainer';
+export { default as ContentHeadline } from './ContentHeadline';
 
 export type * from './ContentHeadline';
 export type * from './ContentContainer';
 
-export * from './useContentContainer';
-export * from './useContentHeadline';
+export { default as useContentContainer } from './useContentContainer';
+export { default as useContentHeadline } from './useContentHeadline';
 
 export type * from './useContentContainer';
 export type * from './useContentHeadline';
